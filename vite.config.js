10c48c7dde926d190ecc9eb0c7cea4f6ec85c61b
Himanshu Parashar <parasharhimanshu41@gmail.com>
2024-09-28 10:18:32 +0530

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Hangman-Game/', // Important for GitHub Pages
-  build: {
-    outDir: 'dist', // Ensure this matches your deploy script
-  }
 })
